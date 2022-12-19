12d1b07bc8238bda924a0ab84707520e9e8db2d8
mvries <m97devries@gmail.com>
2022-12-19 10:36:26 +0100

    Updated make config script to add input output path to config.

--- Pipeline/scripts/make_config.py
+++ Pipeline/scripts/make_config.py
@@ -1,9 +1,10 @@
 #This is a script that is used to write a config file.
 #This config file is used during the execution of the snakemake workflow.
-#The config file contains a list of input samples in .yaml format
-#To run this script: python3 make_config.py sample_dir path_to_yaml
+#The config file contains samples, input and output paths in .yaml format
+#To run this script: python3 make_config.py sample_dir path_to_yaml outdir
+#Sample dir structure: Data/{Sample}/{Sample}.fq
 
-def process_samples(sample_dir, path_to_config):
+def make_config(sample_dir, path_to_config, output_dir):
     #This function is used to process the input samples:
     #Required input: Path to Dir where sample reads are stored
     #Output: List of samples and paths in yaml format
@@ -15,10 +16,10 @@ def process_samples(sample_dir, path_to_config):
     path_list = []
     #Make list to store sample names:
     sample_list = []
-    #Glob is used to generate a list of paths to each sample
-    sample_dir = sample_dir + "*"
-    path_list = glob.glob(sample_dir)
-    print(path_list)
+    #Glob is used to generate a list of paths to each sample in the input dir.
+    #The 'root' path is saved and written to the config file later.
+    sample_dir_full = sample_dir + "*"
+    path_list = glob.glob(sample_dir_full)
 
     #Loop over path list to get the sample names and append to samples list:
     for i in path_list:
@@ -27,12 +28,12 @@ def process_samples(sample_dir, path_to_config):
 
     #Open config file and write in the right format (.yaml):
     with open(path_to_config, 'a') as config_file:
-        config_file.write('samples:\n')
+        config_file.write('SAMPLES:\n')
 
         #Loop over both the sample and path list to write them together:
         for i in range(0, len(sample_list)):
-            #Make empty string with tab to store entry for writing to file:
-            entry = "\t"
+            #Make empty string with space to store entry for writing to file:
+            entry = " "
             #add sample name to entry
             entry += (sample_list[i] + ': ')
             #add path to sample reads to entry
@@ -40,16 +41,28 @@ def process_samples(sample_dir, path_to_config):
             #Write entries to config file:
             config_file.write(entry + "\n")
 
+        #Add empty line to keep the file readable:
+        config_file.write("\n")
+
+        #Add the input and output path to the config file so snakemake can use:
+        config_file.write("INPUT_PATH:\n")
+        config_file.write(f' {sample_dir}\n')
+        config_file.write("OUTPUT_PATH:\n")
+        config_file.write(f' {output_dir}\n')
+
 #Main function:
 def main():
     #Import statement
     from sys import argv
+    import subprocess
     #Establish path to sample dir:
     sample_dir = argv[1]
     #Establish path to config_file
     path_to_config = argv[2]
+    #Establish path to desired output dir:
+    output_dir = argv[3]
     #Run process sample function to write the config file
-    process_samples(sample_dir, path_to_config)
+    make_config(sample_dir, path_to_config, output_dir)
 
 if __name__ == "__main__":
     main()
