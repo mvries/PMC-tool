e5965edfe5febd8b4af5b07a70695b297f293f24
mvries <m97devries@gmail.com>
2022-12-24 12:34:00 +0100

    pipeline now works up to metaspade (single sample assembly) Testing now.

--- Pipeline/PMC.py
+++ Pipeline/PMC.py
@@ -1,19 +1,22 @@
 #This is a python wrapper that controlls the entire workflow.
 #Running this script with the required inputs automatically performs analysis.
-#To use: Python3 PMC.py sample_dir/ Output_dir/ configfile
+#To use: Python3 PMC.py sample_dir/ Output_dir/ configfile phix.fasta plant.fasta
 
 #Import statements:
 from sys import argv
 import subprocess
 from scripts import make_config
 
-#State directories
+#State directories and files needed:
 sample_dir = argv[1]
 output_dir = argv[2]
 configfile = argv[3]
+phix = argv[4]
+plant = argv[5]
+
 
 #Use the make_config script to prepare the config file for execution:
-make_config.make_config(sample_dir, output_dir, configfile)
+make_config.make_config(sample_dir, output_dir, configfile, phix, plant)
 
 #Prepare conda:
 cmd = "conda config --set channel_priority strict"
@@ -21,12 +24,6 @@ subprocess.check_call(cmd, shell=True)
 
 #Run the snekmake workflow:
 cmd = "snakemake --cores 4 --use-conda"
-subprocess.check_call(cmd, shell=True)
-
-#Collect output to desired directory:
-cmd = f"mkdir {output_dir}"
-subprocess.check_call(cmd, shell=True)
-cmd = f"mv {sample_dir}qc/ {output_dir}qc"
 subprocess.check_call(cmd, shell=True)
 
 #Reset config file for next run:
--- Pipeline/scripts/make_config.py
+++ Pipeline/scripts/make_config.py
@@ -1,10 +1,10 @@
 #This is a script that is used to write a config file.
 #This config file is used during the execution of the snakemake workflow.
 #The config file contains samples, input and output paths in .yaml format
-#To run this script: python3 make_config.py sample_dir/ outdir/
+#To run this script: python3 make_config.py sample_dir/ outdir/ phix_file.fa plant_genome.fa
 #Sample dir structure: sample_dir/{Sample}/{Sample}.fq
 
-def make_config(sample_dir, output_dir, configfile):
+def make_config(sample_dir, output_dir, configfile, phix, plant):
     #This function is used to process the input samples:
     #Required input: Path to Dir where sample reads are stored
     #Output: List of samples and paths in yaml format
@@ -12,17 +12,20 @@ def make_config(sample_dir, output_dir, configfile):
     #Import statements:
     import glob
 
-    #Make list to store paths:
-    path_list = []
+    #Make list to store sample paths:
+    sample_path_list = []
     #Make list to store sample names:
     sample_list = []
+    #Make list to store reference genome paths:
+
+
     #Glob is used to generate a list of paths to each sample in the input dir.
     #The 'root' path is saved and written to the config file later.
     sample_dir_full = sample_dir + "*"
-    path_list = glob.glob(sample_dir_full)
+    sample_path_list = glob.glob(sample_dir_full)
 
     #Loop over path list to get the sample names and append to samples list:
-    for i in path_list:
+    for i in sample_path_list:
         sample = i.split("/")[-1]
         sample_list.append(sample)
 
@@ -37,18 +40,26 @@ def make_config(sample_dir, output_dir, configfile):
             #add sample name to entry
             entry += (sample_list[i] + ': ')
             #add path to sample reads to entry
-            entry += (path_list[i])
+            entry += (sample_path_list[i])
             #Write entries to config file:
             config_file.write(entry + "\n")
 
         #Add empty line to keep the file readable:
         config_file.write("\n")
 
-        #Add the input and output path to the config file so snakemake can use:
+        #Add the sample references and output paths to the config file so snakemake can use:
         config_file.write("INPUT_PATH:\n")
         config_file.write(f' {sample_dir}\n')
         config_file.write("OUTPUT_PATH:\n")
         config_file.write(f' {output_dir}\n')
+        config_file.write("PHIX_REF:\n")
+        config_file.write(f' {phix}\n')
+        config_file.write("PLANT_REF:\n")
+        config_file.write(f' {plant}\n')
+        #Add empty line to keep the file readable:
+        config_file.write("\n")
+
+
 
 #Main function:
 def main():
@@ -60,8 +71,12 @@ def main():
     output_dir = argv[2]
     #Establish path to config file:
     config_file = argv[3]
+    #Establish path to Phix ref:
+    phix = argv[4]
+    #Establish path to plant ref:
+    plant = argv[5]
     #Run process sample function to write the config file
-    make_config(sample_dir, output_dir, configfile)
+    make_config(sample_dir, output_dir, configfile, phix, plant)
 
 if __name__ == "__main__":
     main()
