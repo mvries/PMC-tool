f90f3b9047f7dd65c4105eba2f8cb343ef20bb2d
mvries <m97devries@gmail.com>
2023-01-01 17:45:11 +0100

    added SRA support

--- Pipeline/PMC.py
+++ Pipeline/PMC.py
@@ -1,6 +1,6 @@
 #This is a python wrapper that controlls the entire workflow.
 #Running this script with the required inputs automatically performs analysis.
-#To use: Python3 PMC.py sample_dir/ Output_dir/ configfile phix.fasta plant.fasta
+#To use: Python3 PMC.py sample_file configfile phix.fasta plant.fasta output_dir/
 
 #Import statements:
 from sys import argv
@@ -8,15 +8,22 @@ import subprocess
 from scripts import make_config
 
 #State directories and files needed:
-sample_dir = argv[1]
-output_dir = argv[2]
-configfile = argv[3]
-phix = argv[4]
-plant = argv[5]
+sample_file = argv[1]
+configfile = argv[2]
+phix = argv[3]
+plant = argv[4]
+output_dir = argv[5]
 
+
+
+#Reset config file for next run:
+cmd = "rm ../config/config.yaml"
+subprocess.check_call(cmd, shell=True)
+cmd = "cp ../config/config_backup ../config/config.yaml"
+subprocess.check_call(cmd, shell=True)
 
 #Use the make_config script to prepare the config file for execution:
-make_config.make_config(sample_dir, output_dir, configfile, phix, plant)
+make_config.make_config(sample_file, configfile, phix, plant, output_dir)
 
 #Prepare conda:
 cmd = "conda config --set channel_priority strict"
@@ -24,10 +31,4 @@ subprocess.check_call(cmd, shell=True)
 
 #Run the snekmake workflow:
 cmd = "snakemake --cores 4 --use-conda"
-subprocess.check_call(cmd, shell=True)
-
-#Reset config file for next run:
-cmd = "rm ../config/config.yaml"
-subprocess.check_call(cmd, shell=True)
-cmd = "cp ../config/config_backup ../config/config.yaml"
 subprocess.check_call(cmd, shell=True)
--- Pipeline/rules/metaspades.smk
+++ Pipeline/rules/metaspades.smk
@@ -4,11 +4,11 @@ rule metaspades:
     params:
         P=config["PARAMS"]["SPADES"]["P"],
     input:
-        r1=f'{sample_dir}' + "{sample}" + "/{sample}_plant_removed_1.fq.gz",
-        r2=f'{sample_dir}' + "{sample}" + "/{sample}_plant_removed_2.fq.gz",
+        r1=f'{output_dir}' + "sra_download/" + "{sample}/{sample}_plant_removed_1.fq.gz",
+        r2=f'{output_dir}' + "sra_download/" + "{sample}/{sample}_plant_removed_2.fq.gz"
     output:
-        outdir=f'{output_dir}' + "spades/" + "{sample}/",
+        outdir=directory(f'{output_dir}' + "spades/" + "{sample}/"),
     conda:
         "../envs/metaspades.yaml"
     shell:
-        "spades.py --meta -t {params.P} -1 {input.r1} -2 {input.r2} -o {output.outidr}"
+        "spades.py --meta -t {params.P} -1 {input.r1} -2 {input.r2} -o {output.outdir}"
