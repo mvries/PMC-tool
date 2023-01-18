d1cea0c188807bd9a43d03d2ed665c2394b2092a
mvries <m97devries@gmail.com>
2023-01-18 21:39:28 +0100

    removed some text from the merge conflict

--- Pipeline/PMC.py
+++ Pipeline/PMC.py
@@ -1,19 +1,16 @@
-<<<<<<< HEAD
 #This is a python wrapper that controls the PMC workflow.
 #Running this script with the required inputs automatically performs analysis.
 #To use: Python3 PMC.py sample_file phix.fasta plant.fasta output_dir/ ncores Assembly_type
 #Possible assembly types: single (runs metaspades on each sample) co (runs megahit on pooled samples)
-=======
 #This is a python wrapper that controlls the entire workflow.
 #Running this script with the required inputs automatically performs analysis.
 #To use: Python3 PMC.py sample_file configfile phix.fasta plant.fasta output_dir/ ncores
->>>>>>> main
+
 
 #Import statements:
 from sys import argv
 import subprocess
 from scripts import make_config
-<<<<<<< HEAD
 from scripts import sample_pooler
 
 #State directories and files needed:
@@ -40,7 +37,6 @@ configfile = "../config/config.yaml"
 cmd = "rm " + configfile
 subprocess.check_call(cmd, shell=True)
 cmd = "cp ../config/config_backup " + configfile
-=======
 
 #State directories and files needed:
 sample_file = argv[1]
@@ -55,21 +51,15 @@ cores = argv[6]
 cmd = "rm ../config/config.yaml"
 subprocess.check_call(cmd, shell=True)
 cmd = "cp ../config/config_backup ../config/config.yaml"
->>>>>>> main
 subprocess.check_call(cmd, shell=True)
 
 #Use the make_config script to prepare the config file for execution:
 make_config.make_config(sample_file, configfile, phix, plant, output_dir)
 
-<<<<<<< HEAD
-
-=======
->>>>>>> main
 #Prepare conda:
 cmd = "conda config --set channel_priority strict"
 subprocess.check_call(cmd, shell=True)
 
-<<<<<<< HEAD
 """
 #Run the snakemake workflow that downloads and pre processes the input samples:
 cmd = "snakemake --cores " + str(cores) + " --use-conda " + "--snakefile snakefiles/pre_processer.smk"
@@ -91,8 +81,6 @@ else:
     subprocess.check_call(cmd, shell=True)
     cmd = "snakemake --cores " + str(cores) + " --use-conda " + "--snakefile snakefiles/co_assembly.smk"
     subprocess.check_call(cmd, shell=True)
-=======
 #Run the snekmake workflow:
 cmd = "snakemake --cores " + str(cores) + " --use-conda"
 subprocess.check_call(cmd, shell=True)
->>>>>>> main
